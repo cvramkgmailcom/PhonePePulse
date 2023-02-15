26e9f56140e6a89ab05f36c02161ea88156d8162
cvramkgmailcom <63967394+cvramkgmailcom@users.noreply.github.com>
2023-02-15 10:59:57 +0530

    Create clone.py

---
+++ clone.py
@@ -0,0 +1,12 @@
+import os
+import csv
+import json
+import pandas as pd
+
+# Clone the repository using Git
+os.system("git clone https://github.com/PhonePe/pulse.git")
+
+# Change to the repository directory
+os.chdir("pulse/data")
+
+
