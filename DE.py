bf69ef15a48507764c34ec7bc3bbec622ae12e45
cvramkgmailcom <63967394+cvramkgmailcom@users.noreply.github.com>
2023-02-16 20:26:42 +0530

    Update DE.py

--- DE.py
+++ DE.py
@@ -1,3 +1,25 @@
+#### Cloning the data
+import os
+import csv
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+import warnings
+import mysql.connector
+
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Clone the repository using Git
+os.system("git clone https://github.com/PhonePe/pulse.git")
+
+# Change to the repository directory
+os.chdir("pulse/data")
+
+
+####  fetching the transaction data from aggregated
+print(1)
 import pandas as pd
 import os
 import json
@@ -18,7 +40,7 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
         if fp.endswith('.json'):
           files.append(os.path.join(dirpath, filename, ))
           data=pd.read_json(fp)
-          print(data)
+        #   print(data)
 
           # print(data)
           # tf = pd.read_json(fp)
@@ -49,11 +71,13 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
           df['state'] =n_state
           df['year'] =n_year
           df['qtr'] =n_qtr
-          agg_trans = agg_tran.append(df)
+          agg_trans = agg_trans.append(df)
 agg_trans = agg_trans.reindex(columns=['from','to','no_of_days','country','state','year','qtr','name','type','count','amount'])
 agg_trans.to_csv("agg_tran.csv")
 
 
+##### fetching the user data from aggregated
+print(2)
 root_dir = "D:\\IITM\\project\\PhonePe\\pulse\\data\\aggregated\\user\\country\\india"
 
 
@@ -114,6 +138,9 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
           agg_user = agg_user.append(df)
 agg_user.to_csv("agg_user.csv")
 
+
+##### fetching the transaction data from map
+print(3)
 root_dir = "D:\\IITM\\project\\PhonePe\\pulse\\data\\map\\transaction\\hover\\country\\india"
 
 # List to store the full path of all the files
@@ -158,9 +185,17 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
           df['qtr'] =n_qtr
           df['responseTimestamp'] = data['responseTimestamp'][0]
           map_trans = map_trans.append(df)
+map_trans = map_trans.rename(columns={'name':'district'})
+map_trans['district'] = map_trans['district'].str.replace('district', '')
 # fdf = fdf.reindex(columns=['from','to','no_of_days','country','state','year','qtr','name','type','count','amount'])
+
+# print(df)
+
+
 map_trans.to_csv("map_trans.csv")
 
+##### fetching the user data from map
+print(4)
 root_dir = "D:\\IITM\\project\\PhonePe\\pulse\\data\\map\\user\\hover\\country\\india"
 
 # List to store the full path of all the files
@@ -210,3 +245,44 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
           df['qtr'] =n_qtr
           map_user = map_user.append(df)
 map_user.to_csv("map_user.csv")
+
+##### fetching lat_long
+print(5)
+from geopy.geocoders import Nominatim
+
+# getting the lat_long
+geolocator = Nominatim(user_agent="my-app")
+
+def fetch_lat_long(location):
+    try:
+        loc = geolocator.geocode(location)
+        return (loc.latitude, loc.longitude)
+    except:
+        return None
+    
+unique_values  = map_trans[['country', 'state', 'district']].apply(lambda x: tuple(x), axis=1).unique()
+m_locations = pd.DataFrame(list(unique_values), columns=['country', 'state', 'district'])
+m_locations["lat_long"] = m_locations.apply(lambda x: fetch_lat_long(f'{x["district"]}, {x["state"]}, {x["country"]}'), axis=1)
+m_locations["lat_long"] = m_locations["lat_long"].astype(str)
+m_locations
+
+##### updating all 5 dataframes into mysql database as tables
+print(6)
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+# Create a DataFrame
+# Create a connection to the MySQL database
+engine = create_engine('mysql+pymysql://rk:rk123@localhost/guvi_phonePe')
+# Insert the DataFrame into a MySQL table
+print('started')
+print("agg_trans..")
+agg_trans.to_sql(name='agg_trans', con=engine, if_exists='replace', index=False)
+print('agg_user..')
+agg_user.to_sql(name='agg_user', con=engine, if_exists='replace', index=False)
+print('map_trans...')
+map_trans.to_sql(name='map_trans', con=engine, if_exists='replace', index=False)
+print('map_user...')
+map_user.to_sql(name='map_user', con=engine, if_exists='replace', index=False)
+print('map_location...')
+m_locations.to_sql(name='m_locations', con=engine, if_exists='replace', index=False)
