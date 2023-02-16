915b8ea4a7828f40c3c44ed47dd5fbd56b6c1264
cvramkgmailcom <63967394+cvramkgmailcom@users.noreply.github.com>
2023-02-16 20:29:18 +0530

    Update tosql.py

--- tosql.py
+++ tosql.py
@@ -1,3 +1,5 @@
+##### updating all 5 dataframes into mysql database as tables
+print(6)
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
@@ -5,7 +7,14 @@ import pymysql
 # Create a connection to the MySQL database
 engine = create_engine('mysql+pymysql://rk:rk123@localhost/guvi_phonePe')
 # Insert the DataFrame into a MySQL table
+print('started')
+print("agg_trans..")
 agg_trans.to_sql(name='agg_trans', con=engine, if_exists='replace', index=False)
+print('agg_user..')
 agg_user.to_sql(name='agg_user', con=engine, if_exists='replace', index=False)
+print('map_trans...')
 map_trans.to_sql(name='map_trans', con=engine, if_exists='replace', index=False)
+print('map_user...')
 map_user.to_sql(name='map_user', con=engine, if_exists='replace', index=False)
+print('map_location...')
+m_locations.to_sql(name='m_locations', con=engine, if_exists='replace', index=False)
