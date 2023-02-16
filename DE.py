ea13398ec6e4bc641f022bc7357513606bcb460a
cvramkgmailcom <63967394+cvramkgmailcom@users.noreply.github.com>
2023-02-16 20:29:02 +0530

    Update DE.py

--- DE.py
+++ DE.py
@@ -266,23 +266,3 @@ m_locations["lat_long"] = m_locations.apply(lambda x: fetch_lat_long(f'{x["distr
 m_locations["lat_long"] = m_locations["lat_long"].astype(str)
 m_locations
 
-##### updating all 5 dataframes into mysql database as tables
-print(6)
-import pandas as pd
-from sqlalchemy import create_engine
-import pymysql
-# Create a DataFrame
-# Create a connection to the MySQL database
-engine = create_engine('mysql+pymysql://rk:rk123@localhost/guvi_phonePe')
-# Insert the DataFrame into a MySQL table
-print('started')
-print("agg_trans..")
-agg_trans.to_sql(name='agg_trans', con=engine, if_exists='replace', index=False)
-print('agg_user..')
-agg_user.to_sql(name='agg_user', con=engine, if_exists='replace', index=False)
-print('map_trans...')
-map_trans.to_sql(name='map_trans', con=engine, if_exists='replace', index=False)
-print('map_user...')
-map_user.to_sql(name='map_user', con=engine, if_exists='replace', index=False)
-print('map_location...')
-m_locations.to_sql(name='m_locations', con=engine, if_exists='replace', index=False)
